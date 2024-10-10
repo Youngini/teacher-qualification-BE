3a194ffee8293f594aa0f92d8cfa1eb3454e9b30
young-in <hardworkingini@gmail.com>
2024-10-10 20:32:50 +0900

    초기 세팅 완료

---
+++ qualification/src/test/java/com/teacher/qualification/QualificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacher.qualification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
