de888b14d96ec60a8fff4538cd0670cd677ee94b
young-in <hardworkingini@gmail.com>
2024-11-02 17:49:48 +0900

    refactor : 예약어 사용으로 인한 오류 해결

--- qualification/src/main/java/com/teacher/qualification/domain/user/User.java
+++ qualification/src/main/java/com/teacher/qualification/domain/user/User.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "`user`")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
